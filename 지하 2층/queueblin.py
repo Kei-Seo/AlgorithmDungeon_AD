a8634ab5529e57d778f7dc6569fde6cb0d1c46fc
kei_Seo <67668805+Kei-Seo@users.noreply.github.com>
2021-12-29 09:51:26 +0900

    Create queueblin.py

---
+++ 지하 2층/queueblin.py
@@ -0,0 +1,33 @@
+import sys
+n = int(sys.stdin.readline())
+queue = []
+
+for _ in range(n):
+  command = sys.stdin.readline().strip()
+  
+  if command == "pop":
+    if len(queue) == 0:
+      print("-1")
+    else:
+      temp = queue.pop(0)
+      print(temp)
+  elif command == "size":
+    print(len(queue))
+  elif command == "empty":
+    if len(queue) == 0:
+      print("1")
+    else:
+      print("0")
+  elif command == "front":
+    if len(queue) == 0:
+      print("-1")
+    else:
+      print(queue[0])
+  elif command == "back":
+    if len(queue) == 0:
+      print("-1")
+    else:
+      print(queue[len(queue)-1])
+  else:
+    command, num = command.split(" ")
+    queue.append(int(num))
