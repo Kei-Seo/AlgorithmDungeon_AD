fca307c5e2d2d7cbc48370a561ec118f53059472
kei_Seo <67668805+Kei-Seo@users.noreply.github.com>
2022-01-17 09:47:37 +0900

    Create FourSquares.py

---
+++ 지하 묘비/FourSquares.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+dp = [0, 1]
+
+for i in range(2, n+1):
+  minValue = 1e9
+  j = 1
+  while (j**2) <= i:
+    minValue = min(minValue, dp[i-(j**2)])
+    j += 1
+  dp.append(minValue + 1)
+
+print(dp[n])
