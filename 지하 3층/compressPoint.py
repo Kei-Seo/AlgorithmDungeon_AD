86a7541cea9ad127dfa200eb3e6e091de7d1c68d
kei_Seo <67668805+Kei-Seo@users.noreply.github.com>
2022-01-31 23:59:51 +0900

    Create compressPoint.py

---
+++ 지하 3층/compressPoint.py
@@ -0,0 +1,27 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+arr = {}
+m = list(map(int, sys.stdin.readline().split()))
+j = 1
+
+for i in m:
+  arr[j] = i
+  j += 1
+
+arr2 = sorted(arr.items(), key=lambda x: x[1])
+
+res = [0]*(n+1)
+pre = 10e10
+k = 0 
+for y in arr2:
+  if y[1] == pre:
+    res[y[0]] = k-1
+  else:  
+    res[y[0]] = k
+    k += 1
+  pre = y[1]
+
+for i in range(1, n+1):
+  print(res[i], end = ' ')
