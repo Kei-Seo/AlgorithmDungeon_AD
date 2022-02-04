6baf007ea9a539ed6826d7d5769bd1404befe136
kei_Seo <67668805+Kei-Seo@users.noreply.github.com>
2022-02-04 11:26:46 +0900

    Update DSLR.py

--- 지하 3층/DSLR.py
+++ 지하 3층/DSLR.py
@@ -14,7 +14,6 @@ for _ in range(n):
   
   while q:
     tn, ts = q.popleft()
-   
     if tn == target: 
       print(ts)
       break
@@ -23,11 +22,9 @@ for _ in range(n):
     x = tn * 2
     if x > 9999:
       x = x%10000
-
     if visited[x]:
       q.append((x, ts + "D"))
       visited[x] = False
-    
     #S
     y = tn - 1
     if y < 0:
@@ -35,7 +32,6 @@ for _ in range(n):
     if visited[y]:
       q.append((y, ts + "S"))
       visited[y] = False
-
     #L
     j = tn // 1000
     k = tn % 1000  
@@ -53,7 +49,4 @@ for _ in range(n):
       q.append((a, ts+ "R"))
       visited[a] = False
     
-    
-
-  # L R > 2 > -1
-  
+   
